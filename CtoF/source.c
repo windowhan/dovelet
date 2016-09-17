a5a52b536eaa36872fc74ce035a70f3dacdd9eac
windowhan <window__han@naver.com>
2016-09-17 15:40:59 +0900

    2차 commit
    1일에 5개씩 풀어야지 당분간..

---
+++ CtoF/source.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float c = 0;
+	scanf("%f", &c);
+	printf("%.1f\n", (float)1.8*c+32);
+	return 0;
+}
---
+++ q_r/source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	scanf("%d %d", &a, &b);
+	printf("%d %d\n", a/b, a%b);
+	return 0;
+}
